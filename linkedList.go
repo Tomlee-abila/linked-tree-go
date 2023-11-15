14695a225c20e2c6301e33832cd9bd8f7badbcb6
tabila <tomleeabila@gmail.com>
2023-11-15 18:51:05 +0300

    second list

--- linkedList.go
+++ linkedList.go
@@ -4,11 +4,12 @@ import "fmt"
 
 type Node struct {
 	Data int
+	Data2 string
 	Next *Node
 }
 
-func insert(head *Node, data int) *Node {
-	n := &Node{Data: data}
+func insert(head *Node, data int, data2 string) *Node {
+	n := &Node{Data: data, Data2: data2}
 	if head == nil {
 		return n
 	} else {
@@ -19,7 +20,7 @@ func insert(head *Node, data int) *Node {
 
 func PrintList(head *Node) {
 	for head != nil {
-		fmt.Print(head.Data, " -> ")
+		fmt.Print(head.Data, " -> ", head.Data2," -> ")
 		head = head.Next
 	}
 	fmt.Println(nil)
@@ -27,10 +28,10 @@ func PrintList(head *Node) {
 
 func main() {
 	var link *Node
-	link = insert(link, 1)
-	link = insert(link, 2)
-	link = insert(link, 3)
-	link = insert(link, 4)
+	link = insert(link, 1, "Hello")
+	link = insert(link, 2, "how")
+	link = insert(link, 3, "are")
+	link = insert(link, 4, "you")
 
 	PrintList(link)
 }
